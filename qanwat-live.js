5ade5321b97bb9d7d62e4a5c29a5bdd13a716a2f
FadiCraft <crf0582@gmail.com>
2026-05-04 03:12:19 +0300

    Update qanwat-live.js

--- qanwat-live.js
+++ qanwat-live.js
@@ -5,8 +5,8 @@ const path = require('path');
 const sharp = require('sharp');
 const ffmpeg = require('fluent-ffmpeg');
 
-const IMAGE_DIR = './image';
-const JSON_FILE = 'channels.json';
+const IMAGE_DIR = './image2';
+const JSON_FILE = 'channels2.json';
 const GITHUB_RAW_BASE = 'https://raw.githubusercontent.com/FadiCraft/TV_Chaanals/refs/heads/main/';
 
 if (!fs.existsSync(IMAGE_DIR)) fs.mkdirSync(IMAGE_DIR, { recursive: true });
